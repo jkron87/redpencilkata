5299527708a0c8b26d01bbcb068bac67e9fd0f06
Jennifer Kron <jkron@pillartechnology.com>
2017-03-27 11:03:37 -0400

    refactored methods

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -9,9 +9,21 @@ describe("The red pencil promotion", () => {
 
         item.priceChange(-.05);
 
-        expect(item.currentPrice).toBe(9.50);
-        expect(item.originalPrice).toBe(10);
         expect(item.isInPromo).toBe(true);
+    });
+
+    it("Price change reduces current price by appropriate amount", () => {
+        let item = new Item(10);
+        let item2 = new Item(10);
+        let item3 = new Item(10);
+
+        item.priceChange(-.05);
+        item2.priceChange(0);
+        item3.priceChange(-1);
+
+        expect(item.currentPrice).toBe(9.50);
+        expect(item2.currentPrice).toBe(10);
+        expect(item3.currentPrice).toBe(0);
     });
 
     it("Should not be active if price drop is less than 5%", () => {
@@ -19,7 +31,6 @@ describe("The red pencil promotion", () => {
 
         item.priceChange(-.04);
 
-        expect(item.currentPrice).toBe(9.6);
         expect(item.isInPromo).toBe(false);
     });
 
@@ -28,7 +39,6 @@ describe("The red pencil promotion", () => {
 
         item.priceChange(-.31);
 
-        expect(item.currentPrice).toBe(6.9);
         expect(item.isInPromo).toBe(false);
     });
 
@@ -38,8 +48,6 @@ describe("The red pencil promotion", () => {
         item.priceChange(-.20);
         item.priceChange(-.20);
 
-        expect(item.currentPrice).toBe(6.4);
-        expect(item.originalPrice).toBe(10);
         expect(item.promoIsActive()).toBe(false);
     });
 
@@ -49,8 +57,6 @@ describe("The red pencil promotion", () => {
         item.priceChange(-.10);
         item.priceChange(-.10);
 
-        expect(item.currentPrice).toBe(8.1);
-        expect(item.originalPrice).toBe(10);
         expect(item.promoIsActive()).toBe(true);
     });
 
@@ -62,27 +68,28 @@ describe("The red pencil promotion", () => {
         expect(item.lastDateChanged.getDay()).toBe(new Date().getDay());
     });
 
-    it("Should not be active if price has not been stable in last 30 days", () => {
+    it("Should not be active if price has changed in last 30 days", () => {
         let item = new Item(10);
 
         let date = new Date();
-        date.setDate(date.getDate() - 1);
-        item.lastDateChanged = date;
+        item.lastDateChanged = date.setDate(date.getDate() - 1);
+
         item.priceChange(-.2);
 
         expect(item.isInPromo).toBe(false);
     });
 
-    it("Should be active if price has been stable in last 30 days", () => {
+
+    it("Should be active if price has not changed in last 30 days", () => {
         let item = new Item(10);
 
         let date = new Date();
-        date.setDate(date.getDate() - 40);
-        item.lastDateChanged = date;
+        item.lastDateChanged = date.setDate(date.getDate() - 40);
         item.priceChange(-.2);
 
         expect(item.isInPromo).toBe(true);
     });
+
 
 
     it("Should record date promo is started for an item", () => {
@@ -96,26 +103,17 @@ describe("The red pencil promotion", () => {
     it("Should not last longer than 30 days", () => {
         let item1 = new Item(10);
         let item2 = new Item(10);
-        let item3 = new Item(10);
 
         item1.priceChange(-.1);
         item2.priceChange(-.1);
-        item3.priceChange(-.1);
 
         let date = new Date();
-        date.setDate(date.getDate() - 31);
-        item1.promoStarted = date;
+        item1.promoStarted = date.setDate(date.getDate() - 31);
 
-        let date2 = new Date();
-        date2.setDate(date2.getDate() - 30);
-        item2.promoStarted = date2;
-
-        let date3 = new Date();
-        date3.setDate(date3.getDate() - 20);
-        item3.promoStarted = date3;
+        date = new Date();
+        item2.promoStarted = date.setDate(date.getDate() - 30);
 
         expect(item1.promoIsActive()).toBe(false);
-        expect(item2.promoIsActive()).toBe(true);
         expect(item2.promoIsActive()).toBe(true);
 
     });
@@ -138,14 +136,13 @@ describe("The red pencil promotion", () => {
         this.isInPromo = true;
 
         let date = new Date();
-        date.setDate(date.getDate() - 20);
-        item.promoStarted = date;
+        item.promoStarted = date.setDate(date.getDate() - 20);
+
 
         item.priceChange(.1);
 
         expect(item.promoIsActive()).toBe(false);
     });
-
 
 
 });
--- src/app.js
+++ src/app.js
@@ -4,25 +4,24 @@ function Item(price) {
     this.isInPromo = false;
     this.currentPrice = price;
     this.originalPrice = price;
-    this.lastDateChanged = setDateTo31DaysAgo();
-    this.promoStarted = setDateTo31DaysAgo();
+    this.lastDateChanged = 0;
+    this.promoStarted = 0;
 }
 
+
 Item.prototype.priceChange = function (amount) {
-    let priceChangeWithinRange = amount <= -.05 && amount > -.3;
-    let priceStable = this.lastDateChanged <= setDateTo31DaysAgo();
-    let totalReductionDoesNotExceedThreshold = (((this.originalPrice - this.currentPrice) / this.originalPrice) - amount) < .3;
 
-    this.lastDateChanged = new Date();
-    this.reduceBy(amount);
-
-    if (totalReductionDoesNotExceedThreshold) {
-        if (priceChangeWithinRange && priceStable || !priceStable && this.promoStarted === true) {
+    if (priceChangeWithinRange(amount) && totalPriceReductionDoesNotExceedThreshold.call(this, amount)) {
+        if (priceStable.call(this) || !(priceStable.call(this)) && this.promoStarted === true) {
             this.startPromo();
         }
     } else {
         this.endPromo();
     }
+
+    this.reduceBy(amount);
+    this.lastDateChanged = new Date();
+
 };
 
 Item.prototype.promoIsActive = function () {
@@ -48,10 +47,21 @@ Item.prototype.reduceBy = function (amount) {
     this.currentPrice = this.currentPrice * ((1 + amount) * 10) / 10;
 };
 
+function priceChangeWithinRange(amount) {
+    return amount <= -.05 && amount > -.3;
+}
+
 function setDateTo31DaysAgo() {
     let date = new Date();
     date.setDate(date.getDate() - 31);
     return date;
 }
 
+function totalPriceReductionDoesNotExceedThreshold(amount) {
+    return ((this.originalPrice - this.currentPrice) / this.originalPrice) - amount < .3;
+}
+
+function priceStable() {
+    return this.lastDateChanged <= setDateTo31DaysAgo();
+}
 module.exports = Item;
